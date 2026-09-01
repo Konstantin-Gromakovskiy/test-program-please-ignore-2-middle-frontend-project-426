54967c7baef18b9e1433b4d6adee0debc05637de
konstantin <k.hromakovskiy@yandex.ru>
2026-09-01 22:09:58 +0300

    fix(back): configure native import aliases

--- back/app.ts
+++ back/app.ts
@@ -1,17 +1,14 @@
 import Fastify from "fastify";
-import routes from "@/routes/index.js";
+import routes from "#routes/index";
 import { fastifyStatic } from "@fastify/static";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const fastify = Fastify();
 
 // Backend routes
 fastify.register(routes, { prefix: "/api" });
 // Frontend routes
-fastify.register(fastifyStatic, { root: path.join(__dirname, "../front/dist"), prefix: "/" });
+fastify.register(fastifyStatic, { root: path.resolve(process.cwd(), "../front/dist"), prefix: "/" });
 
 fastify.setNotFoundHandler((request, reply) => {
   if (request.url.startsWith("/api")) return reply.code(404).send();
