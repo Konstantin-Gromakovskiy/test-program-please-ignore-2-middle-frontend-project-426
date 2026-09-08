0761489f00bae3ed920bbeca300941f0d4b2530f
konstantin <k.hromakovskiy@yandex.ru>
2026-09-08 18:47:13 +0300

    feat: добавил обработку ошибок на беке через bugsink

--- back/app.ts
+++ back/app.ts
@@ -1,12 +1,26 @@
+import * as Sentry from "@sentry/node";
 import Fastify from "fastify";
 import routes from "#routes/index";
 import { fastifyStatic } from "@fastify/static";
 import path from "path";
 
-const fastify = Fastify();
+const SENTRY_DSN = process.env["SENTRY_DSN"];
+const NODE_ENV = process.env["NODE_ENV"];
 const PORT = process.env["PORT"];
 
+if (!SENTRY_DSN) throw new Error("SENTRY_DSN is not set");
+if (!NODE_ENV) throw new Error("NODE_ENV is not set");
 if (!PORT) throw new Error("PORT is not set");
+
+type ErrorWithStatusCode = Error & { statusCode: number };
+
+function isErrorWithStatusCode(error: unknown): error is ErrorWithStatusCode {
+  return error instanceof Error && "statusCode" in error && typeof error.statusCode === "number";
+}
+
+Sentry.init({ dsn: SENTRY_DSN, environment: NODE_ENV });
+
+const fastify = Fastify({ logger: true });
 
 // Backend routes
 fastify.register(routes, { prefix: "/api" });
@@ -22,9 +36,25 @@ fastify.setNotFoundHandler((request, reply) => {
   return reply.sendFile("index.html");
 });
 
+fastify.setErrorHandler((error, _, reply) => {
+  const statusCode = isErrorWithStatusCode(error) ? error.statusCode : 500;
+
+  if (statusCode >= 500) Sentry.captureException(error);
+
+  const errorMessage =
+    statusCode >= 500
+      ? "Internal Server Error"
+      : error instanceof Error
+        ? error.message
+        : "Request failed";
+
+  reply.code(statusCode).send({ error: errorMessage });
+});
+
 fastify.listen({ host: "0.0.0.0", port: Number(PORT) }, (err, address) => {
   if (err) {
     console.error(err);
+    Sentry.captureException(err);
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);
