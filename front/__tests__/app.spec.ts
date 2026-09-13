06cbda291a40956208f303e2813fcec80b6beffd
konstantin <k.hromakovskiy@yandex.ru>
2026-09-13 13:01:26 +0300

    feat(test): написан первый smoke-test

--- front/__tests__/app.spec.ts
+++ front/__tests__/app.spec.ts
@@ -2,16 +2,6 @@ import { expect, test } from '@playwright/test'
 
 test('renders the main UI', async ({ page }) => {
   await page.goto('/')
-
-  await expect(page.getByRole('heading', { name: 'Get started' })).toBeVisible()
-  await expect(page.getByRole('button', { name: 'Count is 0' })).toBeVisible()
+  await expect(page).toHaveTitle('front')
 })
 
-test('increments the counter', async ({ page }) => {
-  await page.goto('/')
-
-  const counter = page.getByRole('button', { name: 'Count is 0' })
-  await counter.click()
-
-  await expect(page.getByRole('button', { name: 'Count is 1' })).toBeVisible()
-})
